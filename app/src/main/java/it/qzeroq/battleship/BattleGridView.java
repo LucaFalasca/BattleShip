fe1bd0adcbde17e3b63e6c1a02eb7c67c9e2060a
LucaF <lucafalasca08@gmail.com>
2020-06-09 18:08:00 +0200

    Prima versione della grid view

--- app/src/main/java/it/qzeroq/battleship/BattleGridView.java
+++ app/src/main/java/it/qzeroq/battleship/BattleGridView.java
@@ -1,35 +1,25 @@
 package it.qzeroq.battleship;
 
 import android.content.Context;
-import android.content.res.Resources;
-import android.content.res.TypedArray;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
-import android.graphics.Color;
-import android.graphics.Matrix;
-import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.Gravity;
 import android.view.View;
 import android.widget.GridLayout;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.TableLayout;
-import android.widget.TableRow;
 import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 
-import com.squareup.picasso.Callback;
-import com.squareup.picasso.Picasso;
+public class BattleGridView extends GridLayout {
 
-import org.w3c.dom.Attr;
-
-public class BattleGridView extends LinearLayout {
-
-    int side;
+    final int CELL_SIDE = 50;
+    final int GRID_SIZE = 10;
     Context context;
+    ImageView[][] cells = new ImageView[GRID_SIZE][GRID_SIZE];
+    TextView[] columns = new TextView[GRID_SIZE];
+    TextView[] rows = new TextView[GRID_SIZE];
+    TextView uselessCell;
 
     public BattleGridView(Context context) {
         super(context);
@@ -43,53 +33,74 @@ public class BattleGridView extends LinearLayout {
         init();
     }
 
-
-
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
-    }
-
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
         super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+        int side;
         if(getMeasuredWidth() > getMeasuredHeight()){
-            side = getMeasuredHeight() / 11;
+            side = getMeasuredHeight() / (GRID_SIZE + 1);
         }
+        else{
+            side = getMeasuredWidth() / (GRID_SIZE + 1);
+        }
+        setSizeCells(side);
     }
 
     private void init(){
-        this.setOrientation(VERTICAL);
-        int dstWidth = 0,dstHeight = 0;
-        ImageView[][] img = new ImageView[10][10];
-        for(int i = 0; i < 10; i++) {
-            LinearLayout t = new LinearLayout(context);
+        int realSize = GRID_SIZE + 1;
+        this.setAlignmentMode(GridLayout.ALIGN_MARGINS);
+        this.setColumnCount(realSize);
+        this.setRowCount(realSize);
 
-            for(int j = 0; j < 10; j++) {
-                img[i][j] = new ImageView(context);
-                //img[i][j].setText(i + " " + j + "|");
-
-                Resources res = getContext().getResources();
-                BitmapFactory.Options options = new BitmapFactory.Options();
-                options.inJustDecodeBounds = true;
-                BitmapFactory.decodeResource(res, R.drawable.ic_prova, options);
-                options.inJustDecodeBounds = false;
-                Matrix matrix = new Matrix();
-                matrix.postScale(0.5f, 0.5f);
-                options.inSampleSize = calculateSampleSize(options.outWidth, options.outHeight, 100, 100);
-                Bitmap b = BitmapFactory.decodeResource(res, R.drawable.ic_prova, options);
-                b = Bitmap.createBitmap(b, 100, 100, 100, 100, matrix, true);
-                img[i][j].setImageBitmap(b);
-
-                img[i][j].setImageResource(R.drawable.ic_prova);
-                t.addView(img[i][j]);
-
-                //img[i][j].setLayoutParams(layoutParams);
-
+        for(int i = 0; i < GRID_SIZE; i++) {
+            for(int j = 0; j < GRID_SIZE; j++) {
+                if (i == 0 && j == 0) {
+                    uselessCell = new TextView(context);
+                    this.addView(uselessCell);
+                    for(int k = 0; k < GRID_SIZE; k++) {
+                        columns[k] = new TextView(context);
+                        columns[k].setText(String.valueOf(k + 1));
+                        columns[k].setGravity(View.TEXT_ALIGNMENT_GRAVITY);
+                        this.addView(columns[k]);
+                    }
+                }
+                if(j == 0){
+                    rows[i] = new TextView(context);
+                    rows[i].setText(Character.toString((char) (65 + i)));
+                    rows[i].setGravity(View.TEXT_ALIGNMENT_GRAVITY);
+                    this.addView(rows[i]);
+                }
+                cells[i][j] = new ImageView(context);
+                cells[i][j].setImageResource(R.drawable.ic_prova);
+                this.addView(cells[i][j]);
             }
-            this.addView(t);
+        }
+        setSizeCells(CELL_SIDE);
+    }
+
+    private void setSizeCells(int side) {
+
+        for(int i = 0; i < 10; i++) {
+            for (int j = 0; j < 10; j++) {
+                GridLayout.LayoutParams param = new GridLayout.LayoutParams();
+                param.height = side ;
+                param.width = side;
+                cells[i][j].setLayoutParams(param);
+            }
+        }
+        for(int i = 0; i < GRID_SIZE; i++){
+            GridLayout.LayoutParams param = new GridLayout.LayoutParams();
+            param.height = side;
+            param.width = side;
+            columns[i].setLayoutParams(param);
         }
 
+        for(int i = 0; i < GRID_SIZE; i++){
+            GridLayout.LayoutParams param = new GridLayout.LayoutParams();
+            param.height = side ;
+            param.width = side;
+            rows[i].setLayoutParams(param);
+        }
     }
 
 }
