45db7944935170a6fb8936c168d2774c86c132e7
Andrea <andrea.mancuso098@gmail.com>
2020-06-09 22:25:17 +0200

    Aggiunto il menu iniziale da sistemare

---
+++ app/src/main/java/it/qzeroq/battleship/BattleGrid.java
@@ -0,0 +1,14 @@
+package it.qzeroq.battleship;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BattleGrid extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.grid_layout);
+    }
+}
--- app/src/main/java/it/qzeroq/battleship/MainActivity.java
+++ app/src/main/java/it/qzeroq/battleship/MainActivity.java
@@ -2,7 +2,10 @@ package it.qzeroq.battleship;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Holder holder = new Holder();
+    }
+
+    class Holder implements View.OnClickListener {
+        Button btnStart;
+        Button btnSetting;
+
+        Holder(){
+            btnStart = findViewById(R.id.btnStart);
+            btnSetting = findViewById(R.id.btnSetting);
+
+            btnStart.setOnClickListener(this);
+            btnSetting.setOnClickListener(this);
+        }
+
+        @Override
+        public void onClick(View v) {
+            if(v.getId() == R.id.btnStart){
+                Intent intent = new Intent(MainActivity.this, BattleGrid.class);
+                startActivity(intent);
+            }
+            else{
+                Intent intent = new Intent(MainActivity.this, Settings.class);
+                startActivity(intent);
+            }
+        }
     }
 }
